32fc6a3aaf07fe87732d0db112aa08e07d19363f
Terrorbladezz <anirudhsriram1995@gmail.com>
2020-12-31 16:40:21 +0530

    Updates

---
+++ precache-manifest.06134ca17481092f35ad672ac4b51b9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877529b4cf75c7981ce374a80a324c25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a2ce442d975305a9e5",
+    "url": "/static/css/main.0fb198a1.chunk.css"
+  },
+  {
+    "revision": "205d363cab7a333f94ef",
+    "url": "/static/js/2.cda4d5b2.chunk.js"
+  },
+  {
+    "revision": "60a2ce442d975305a9e5",
+    "url": "/static/js/main.f7c3cd38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
